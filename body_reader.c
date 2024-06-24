1bd942268dec9d8c96d6bbc0addb3e9c610b33a8
Janitra Panji <bonkzero404@gmail.com>
2024-06-24 23:15:35 +0700

    Add tcp socket

--- body_reader.c
+++ body_reader.c
@@ -1,4 +1,5 @@
 #include "body_reader.h"
+#include "http_log.h"
 
 keyValuePair *readBody(request_rec *r) {
     apr_array_header_t *pairs = NULL;
@@ -9,8 +10,15 @@ keyValuePair *readBody(request_rec *r) {
     char *buffer;
     keyValuePair *kvp;
 
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "mod_redsec_terminator: Calling ap_parse_form_data");
     res = ap_parse_form_data(r, NULL, &pairs, -1, HUGE_STRING_LEN);
-    if (res != OK || !pairs) return NULL; /* Return NULL if we failed or if there is no POST data */
+    if (res != OK || !pairs) {
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "mod_redsec_terminator: Failed to parse form data or no data present");
+        return NULL; /* Return NULL if we failed or if there is no POST data */
+    }
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "mod_redsec_terminator: Form data parsed successfully");
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "mod_redsec_terminator: Number of pairs: %d", pairs->nelts);
+
     kvp = apr_pcalloc(r->pool, sizeof(keyValuePair) * (pairs->nelts + 1));
     while (pairs && !apr_is_empty_array(pairs)) {
         ap_form_pair_t *pair = (ap_form_pair_t *) apr_array_pop(pairs);
@@ -21,6 +29,7 @@ keyValuePair *readBody(request_rec *r) {
         buffer[len] = 0;
         kvp[i].key = apr_pstrdup(r->pool, pair->name);
         kvp[i].value = buffer;
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "mod_redsec_terminator: Read key-value pair: %s = %s", kvp[i].key, kvp[i].value);
         i++;
     }
     return kvp;
--- mod_redsec_terminator.c
+++ mod_redsec_terminator.c
@@ -4,7 +4,79 @@
 #include "json-c/json.h"
 #include "body_reader.h"
 #include "json_reader.h"
+#include "http_log.h"
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+
+static int send_to_tcp_socket(const char *url, const char *data) {
+    int sockfd, status;
+    struct addrinfo hints, *res, *p;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; // Use IPv4 or IPv6, whichever is available
+    hints.ai_socktype = SOCK_STREAM;
+
+    // Parse URL to get host and port
+    char *hostname;
+    char *port_str;
+    char *url_copy = apr_pstrdup(apr_hook_global_pool, url);
+
+    if (strncmp(url_copy, "http://", 7) == 0) {
+        url_copy += 7;
+    } else if (strncmp(url_copy, "https://", 8) == 0) {
+        url_copy += 8;
+    }
+
+    hostname = strtok(url_copy, ":/");
+    port_str = strtok(NULL, ":/");
+
+    if (port_str == NULL) {
+        port_str = "80";
+    }
+
+    if ((status = getaddrinfo(hostname, port_str, &hints, &res)) != 0) {
+        ap_log_error(APLOG_MARK, APLOG_ERR, 0, NULL, "mod_redsec_terminator: getaddrinfo error: %s", gai_strerror(status));
+        return -1;
+    }
+
+    // Loop over all the results and connect to the first we can
+    for (p = res; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            ap_log_error(APLOG_MARK, APLOG_ERR, 0, NULL, "mod_redsec_terminator: Error opening socket");
+            continue;
+        }
+
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            ap_log_error(APLOG_MARK, APLOG_ERR, 0, NULL, "mod_redsec_terminator: Error connecting to socket");
+            continue;
+        }
+
+        break; // Connected successfully
+    }
+
+    if (p == NULL) {
+        ap_log_error(APLOG_MARK, APLOG_ERR, 0, NULL, "mod_redsec_terminator: Failed to connect to socket");
+        return -1;
+    }
+
+    freeaddrinfo(res); // All done with this structure
+
+    // Now send data to the socket
+    int bytes_sent = send(sockfd, data, strlen(data), 0);
+    if (bytes_sent == -1) {
+        ap_log_error(APLOG_MARK, APLOG_ERR, 0, NULL, "mod_redsec_terminator: Error sending data");
+        close(sockfd);
+        return -1;
+    }
+
+    close(sockfd);
+    return 0;
+}
 
 static int mod_redsec_terminator_handler(request_rec *r) {
     if (apr_strnatcasecmp(r->handler, "mod_redsec_terminator")) {
@@ -14,21 +86,41 @@ static int mod_redsec_terminator_handler(request_rec *r) {
     const char *content_type = apr_table_get(r->headers_in, "Content-Type");
 
     if (content_type) {
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "mod_redsec_terminator: Content-Type: %s", content_type);
         r->content_type = apr_pstrdup(r->pool, content_type);
     } else {
         r->content_type = "text/html";
     }
 
-    ap_rprintf(r, "Query Parameters:\n");
+    const char *url_socket = "http://localhost:8585"; // Destination URL for socket
+
+    json_object *json_obj = json_object_new_object();
+    json_object *query_params_obj = json_object_new_object();
+    json_object *body_obj = json_object_new_object();
+
+    // Parse query parameters
     if (r->args) {
-        ap_rprintf(r, "%s\n", r->args);
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "mod_redsec_terminator: Query parameters: %s", r->args);
+
+        char *query_params = apr_pstrdup(r->pool, r->args);
+        char *param_name = strtok(query_params, "&?");
+
+        while (param_name != NULL) {
+            char *param_value = strchr(param_name, '=');
+            if (param_value) {
+                *param_value++ = '\0';
+                json_object_object_add(query_params_obj, param_name, json_object_new_string(param_value));
+            } else {
+                json_object_object_add(query_params_obj, param_name, json_object_new_string(""));
+            }
+            param_name = strtok(NULL, "&?");
+        }
     } else {
-        ap_rprintf(r, "No query parameters.\n");
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "mod_redsec_terminator: No query parameters");
     }
 
-    // Print request body
-    ap_rputs("\nTes Request Body:\n", r);
-    if (r->method_number == M_POST || r->method_number == M_PUT) {
+    // Process request body
+    if (r->method_number == M_POST || r->method_number == M_PUT || r->method_number == M_PATCH || r->method_number == M_DELETE || r->method_number == M_GET) {
         keyValuePair *formData;
         if (apr_strnatcasecmp(r->content_type, "application/json") == 0) {
             formData = readJson(r);
@@ -36,22 +128,34 @@ static int mod_redsec_terminator_handler(request_rec *r) {
             formData = readBody(r);
         }
         if (formData) {
-            int i;
-            for (i = 0; &formData[i]; i++) {
-                if (formData[i].key && formData[i].value) {
-                    ap_rprintf(r, "%s = %s\n", formData[i].key, formData[i].value);
-                } else if (formData[i].key) {
-                    ap_rprintf(r, "%s\n", formData[i].key);
-                } else if (formData[i].value) {
-                    ap_rprintf(r, "= %s\n", formData[i].value);
-                } else {
-                    break;
-                }
+            for (int i = 0; formData[i].key || formData[i].value; i++) {
+                json_object_object_add(body_obj, formData[i].key ? formData[i].key : "", json_object_new_string(formData[i].value ? formData[i].value : ""));
             }
         }
-    } else {
-        ap_rputs("Method is not POST or PUT.\n", r);
     }
+
+    // Add query_params, body, and url to the json_obj
+    json_object_object_add(json_obj, "query_params", query_params_obj);
+    json_object_object_add(json_obj, "body", body_obj);
+	if (strncmp(r->protocol, "HTTP/1.1", 8) == 0) {
+		json_object_object_add(json_obj, "protocol", json_object_new_string("http"));
+	} else if (strncmp(r->protocol, "HTTP/2", 6) == 0) {
+		json_object_object_add(json_obj, "protocol", json_object_new_string("https"));
+	}
+    json_object_object_add(json_obj, "host", json_object_new_string(r->hostname));
+	json_object_object_add(json_obj, "uri", json_object_new_string(r->uri));
+	json_object_object_add(json_obj, "method", json_object_new_string(r->method));
+	json_object_object_add(json_obj, "remote_ip", json_object_new_string(r->useragent_ip));
+
+    const char *json_str = json_object_to_json_string(json_obj);
+
+    // Send data to TCP socket
+    send_to_tcp_socket(url_socket, json_str);
+
+    // Cleanup JSON objects
+    json_object_put(json_obj);
+    json_object_put(query_params_obj);
+    json_object_put(body_obj);
 
     return OK;
 }
