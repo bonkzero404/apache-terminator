b9880ae615abc9cc403717eb8bf0f6f0b53cf0fd
Janitra Panji <bonkzero404@gmail.com>
2024-06-25 14:42:09 +0700

    fixed untracked files

--- bak/bak.c
+++ bak/bak.c
@@ -1,7 +1,5 @@
-#include "httpd.h"
-#include "http_config.h"
+#include "http_core.h"
 #include "http_protocol.h"
-#include "ap_config.h"
 #include "apr_strings.h"
 #include "json-c/json.h"
 #include "body_reader.h"
@@ -14,35 +12,15 @@
 #include <netdb.h>
 #include <unistd.h>
 
-module AP_MODULE_DECLARE_DATA mod_redsec_terminator_module;
-char *url = NULL;
-
-typedef struct {
-    const char *socket_url;
-} mod_redsec_terminator_config;
-
-static void *create_mod_redsec_terminator_config(apr_pool_t *p, char *dir) {
-    mod_redsec_terminator_config *config = (mod_redsec_terminator_config *)apr_pcalloc(p, sizeof(mod_redsec_terminator_config));
-    config->socket_url = NULL; // Initialize socket_url to NULL
-    return (void *)config;
-}
-
-static const char *set_socket_url(cmd_parms *cmd, void *cfg, const char *arg) {
-    mod_redsec_terminator_config *config = (mod_redsec_terminator_config *)cfg;
-    config->socket_url = apr_pstrdup(cmd->pool, arg);
-	ap_log_error(APLOG_MARK, APLOG_ERR, 0, NULL, "mod_redsec_terminator: Socket URL set to %s", config->socket_url);
-	url = apr_pstrdup(cmd->pool, arg);
-    return NULL;
-}
-
 static int send_to_tcp_socket(const char *url, const char *data) {
     int sockfd, status;
     struct addrinfo hints, *res, *p;
 
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
+    hints.ai_family = AF_UNSPEC; // Use IPv4 or IPv6, whichever is available
     hints.ai_socktype = SOCK_STREAM;
 
+    // Parse URL to get host and port
     char *hostname;
     char *port_str;
     char *url_copy = apr_pstrdup(apr_hook_global_pool, url);
@@ -65,6 +43,7 @@ static int send_to_tcp_socket(const char *url, const char *data) {
         return -1;
     }
 
+    // Loop over all the results and connect to the first we can
     for (p = res; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             ap_log_error(APLOG_MARK, APLOG_ERR, 0, NULL, "mod_redsec_terminator: Error opening socket");
@@ -77,7 +56,7 @@ static int send_to_tcp_socket(const char *url, const char *data) {
             continue;
         }
 
-        break;
+        break; // Connected successfully
     }
 
     if (p == NULL) {
@@ -85,8 +64,9 @@ static int send_to_tcp_socket(const char *url, const char *data) {
         return -1;
     }
 
-    freeaddrinfo(res);
+    freeaddrinfo(res); // All done with this structure
 
+    // Now send data to the socket
     int bytes_sent = send(sockfd, data, strlen(data), 0);
     if (bytes_sent == -1) {
         ap_log_error(APLOG_MARK, APLOG_ERR, 0, NULL, "mod_redsec_terminator: Error sending data");
@@ -103,23 +83,22 @@ static int mod_redsec_terminator_handler(request_rec *r) {
         return DECLINED;
     }
 
-    // mod_redsec_terminator_config *config = ap_get_module_config(r->server->module_config, &mod_redsec_terminator_module);
-	// if (!config || !config->socket_url) {
-	// 	ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "mod_redsec_terminator: Configuration not initialized properly");
-	// 	return HTTP_INTERNAL_SERVER_ERROR;
-	// }
+    const char *content_type = apr_table_get(r->headers_in, "Content-Type");
 
-	if (!url) {
-		ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "mod_redsec_terminator: Configuration not initialized properly");
-		return HTTP_INTERNAL_SERVER_ERROR;
-	}
+    if (content_type) {
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "mod_redsec_terminator: Content-Type: %s", content_type);
+        r->content_type = apr_pstrdup(r->pool, content_type);
+    } else {
+        r->content_type = "text/html";
+    }
 
-    const char *url_socket = url;
+    const char *url_socket = "http://localhost:8585"; // Destination URL for socket
 
     json_object *json_obj = json_object_new_object();
     json_object *query_params_obj = json_object_new_object();
     json_object *body_obj = json_object_new_object();
 
+    // Parse query parameters
     if (r->args) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "mod_redsec_terminator: Query parameters: %s", r->args);
 
@@ -140,6 +119,7 @@ static int mod_redsec_terminator_handler(request_rec *r) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "mod_redsec_terminator: No query parameters");
     }
 
+    // Process request body
     if (r->method_number == M_POST || r->method_number == M_PUT || r->method_number == M_PATCH || r->method_number == M_DELETE || r->method_number == M_GET) {
         keyValuePair *formData;
         if (apr_strnatcasecmp(r->content_type, "application/json") == 0) {
@@ -154,22 +134,25 @@ static int mod_redsec_terminator_handler(request_rec *r) {
         }
     }
 
+    // Add query_params, body, and url to the json_obj
     json_object_object_add(json_obj, "query_params", query_params_obj);
     json_object_object_add(json_obj, "body", body_obj);
-    if (strncmp(r->protocol, "HTTP/1.1", 8) == 0) {
-        json_object_object_add(json_obj, "protocol", json_object_new_string("http"));
-    } else if (strncmp(r->protocol, "HTTP/2", 6) == 0) {
-        json_object_object_add(json_obj, "protocol", json_object_new_string("https"));
-    }
+	if (strncmp(r->protocol, "HTTP/1.1", 8) == 0) {
+		json_object_object_add(json_obj, "protocol", json_object_new_string("http"));
+	} else if (strncmp(r->protocol, "HTTP/2", 6) == 0) {
+		json_object_object_add(json_obj, "protocol", json_object_new_string("https"));
+	}
     json_object_object_add(json_obj, "host", json_object_new_string(r->hostname));
-    json_object_object_add(json_obj, "uri", json_object_new_string(r->uri));
-    json_object_object_add(json_obj, "method", json_object_new_string(r->method));
-    json_object_object_add(json_obj, "remote_ip", json_object_new_string(r->useragent_ip));
+	json_object_object_add(json_obj, "uri", json_object_new_string(r->uri));
+	json_object_object_add(json_obj, "method", json_object_new_string(r->method));
+	json_object_object_add(json_obj, "remote_ip", json_object_new_string(r->useragent_ip));
 
     const char *json_str = json_object_to_json_string(json_obj);
 
+    // Send data to TCP socket
     send_to_tcp_socket(url_socket, json_str);
 
+    // Cleanup JSON objects
     json_object_put(json_obj);
     json_object_put(query_params_obj);
     json_object_put(body_obj);
@@ -181,18 +164,12 @@ static void mod_redsec_terminator_register_hooks(apr_pool_t *p) {
     ap_hook_handler(mod_redsec_terminator_handler, NULL, NULL, APR_HOOK_MIDDLE);
 }
 
-static const command_rec mod_redsec_terminator_cmds[] = {
-    AP_INIT_TAKE1("RedSecTerminatorURLSocket", set_socket_url, NULL, RSRC_CONF,
-                  "Specify the custom socket URL for data transmission"),
-    {NULL}
-};
-
 module AP_MODULE_DECLARE_DATA mod_redsec_terminator_module = {
     STANDARD20_MODULE_STUFF,
-    create_mod_redsec_terminator_config,
-    NULL,
-    NULL,
-    NULL,
-    mod_redsec_terminator_cmds,
-    mod_redsec_terminator_register_hooks
+    NULL,            /* Per-directory configuration handler */
+    NULL,            /* Merge handler for per-directory configurations */
+    NULL,            /* Per-server configuration handler */
+    NULL,            /* Merge handler for per-server configurations */
+    NULL,            /* Any directives we may have for httpd */
+    mod_redsec_terminator_register_hooks   /* register hooks */
 };
